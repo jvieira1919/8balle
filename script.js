d342978fd08424ef8b1cf4f8c9922764e528edf1
JV12 <jvieira1919@gmail.com>
2020-04-12 20:54:36 -0400

    update js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var specialChara = "!@#$%^&*()";
+
+var special_chars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+var upper = ["A", "B", "C", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lower = ["a", "b", "c", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
@@ -15,93 +19,66 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   var generatedPassword = "";
-  var passwordLength = prompt("Enter password length. No less than 8 and no more than 128 characters.");
+  var char_superset = [];
+  var passwordLength = parseInt(prompt("Enter password length. No less than 8 and no more than 128 characters."));
+  // validate the user entered a number between 8 and 128
   var passQ1 = confirm("Do you want special charactors?");
+  // confirm (yes/no) if the user wants special characters
   var passQ2 = confirm("Do you want uppercase?");
+  // confirm (yes/no) if the user wants Upper case letters
   var passQ3 = confirm("Do you want lowercase?");
-  var passQ4 = confirm("Do you want numbers?");
-  var minCount = 8;
-  var minNumb = "";
-  var minLower = "";
-  var minUpper = "";
-  var minSymbol = "";
-  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var lower = "abcdefghijklmnopqrstuvwxyz";
-  var numb = "123456789";
-  var specialChara = "!@#$%^&*()";
-  var usercharacters = "";
+  // confirm (yes/no) if the user wants lower case letters
+  var passQ4 = confirm("Do you want numerics?");
+  // confirm (yes/no) if the user wants numerics
+  // validate the user chose at least one set of alphanumerics
 
-  return generatedPassword
-}
-console.log(usercharacters);
+  // create a superset of characters.
+  // if the user wants special characters. concat special_chars to the end of char_superset
+  // repeat for upper, lower and numbers
+  if (passQ1) {
+    char_superset = char_superset.concat(special_chars)
+  };
 
-// check if user wants special characters
-//add special characters to our user characters var
+  if (passQ2) {
+    char_superset = char_superset.concat(upper)
+  };
 
-var functionArray = {
-  //http://www.net-comber.com/charset.html
-  getLowerRandom: function () {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-  },
+  if (passQ3) {
+    char_superset = char_superset.concat(lower)
+  };
 
-  GetUpperRandom: function () {
-    return string.fromCharCode(Math.floor(Math.random() * 26) + 65);
-  },
+  if (passQ4) {
+    char_superset = char_superset.concat(numeric)
+  };
 
-  getRandomNumber: function () {
-    return string.fromCharCode(math.floor(math.random() * 10) + 48);
-  },
+  if (parseInt(passwordLength) < 8 || parseInt(passwordLength) > 128) {
+    alert("Your password must contain a number between 8 and 128")
+    return;
+  };
 
-  GetSymbolRandom: function () {
-    return upper.fromCharCode(Math.floor(Math.random() * specialChara, length));
-  },
-}
+  if ({ passQ1 } && { passQ2 } && { passQ3 } && { passQ4 } === false) {
+    alert("You must choose one of the four options")
+    return;
+  };
 
-// check if the user wants upper case chara
-// add upper case
+  function UserInput(generatedPassword) {
+    document.getElementById("password").textContent = generatedPassword;
+  };
 
-// check the length of usercharacters that its longer than 0
-// if its equal to 0 prompt the user "a message to add"
+  for (var i = 0; i < passwordLength; i++) {
+    generatedPassword = char_superset[Math.floor(Math.random() * passwordLength.length)];
+    char_superset.push(generatedPassword);
+  }
 
-// check if the length is between 8 and 128
-// if thats false prompt user to enter something thats between those.
-
-// create a loop for each character you want to add. 
-// generate a letter randomly from your userchara apend that to the password sting.+
-
-
-
-
-for (var i = 0; i < password.length; i++) {
-  console.log(password[i]);
+  char_superset = generatedPassword.join("");
+  UserInput(generatedPassword);
+  return generatedPassword;
 }
 
-if (passQ1 === true) {
-  minSymbol = functionArray.GetSymbolRandom();
-  minCount++;
-}
-
-else if (passQ2 === true) {
-  minLower = functionArray.getUpperRandom();
-  minCount++;
-}
-
-else if (passQ3 === true) {
-  minUpper = functionArray.GetLowerRandom();
-  minCount++;
-}
-
-else if (passQ4 === true) {
-  minNumb = functionArray.getRandomNumber();
-  minCount++;
-}
-
-else(passQ1, passQ2, passQ3, passQ4 ===false ) {
-  alert("you must choose at least one of the options")
-}
-
-if (password[i].length < (8) || password[i].length > 128)
-  alert("your password must have more than 8 charactors or less than 128")
+// This puts the password value into the textbox
 
 
-
+    //find a random number between 0 and the length of char_superset 
+    //that number represents and index in char_superset
+    //which corresponds to a character
+    //add the character at that index to the end of generatedpassword
